68a006b1a27aefd0ee7b89778088d2bca344b609
omer_sm <smorodinskyomer@gmail.com>
2025-06-14 14:09:46 +0300

    feat(message_list.ex): add sender name and time to messages

--- frontend/lib/components/chat/message_list.ex
+++ frontend/lib/components/chat/message_list.ex
@@ -20,12 +20,35 @@ defmodule Frontend.Components.Chat.MessageList do
   end
 
   def message_item(message_data, user_id) do
+    {:ok, message_time, _} = DateTime.from_iso8601(message_data["created_at"])
+    now = DateTime.now!("Etc/UTC")
+    is_today =
+      message_time.year == now.year and
+      message_time.month == now.month and
+      message_time.day == now.day
+
+    message_time_formatted =
+      if is_today do
+        # Format as HH:mm
+        "#{pad2(message_time.hour)}:#{pad2(message_time.minute)}"
+      else
+        # Format as yy/MM/dd HH:mm
+        "#{rem(message_time.year, 100) |> pad2()}/#{pad2(message_time.month)}/#{pad2(message_time.day)} #{pad2(message_time.hour)}:#{pad2(message_time.minute)}"
+      end
+
     ~s"""
     <div class="chat chat-#{if message_data["user_id"] == user_id, do: "end", else: "start"}">
+      <div class="chat-header text-sm">
+        #{if message_data["user_id"] == user_id, do: "You", else: message_data["sender_name"]}
+        <time class="text-sm opacity-50">#{message_time_formatted}</time>
+      </div>
       <div class="chat-bubble">
         #{message_data["content"]}
       </div>
     </div>
     """
   end
+
+  defp pad2(n) when is_integer(n) and n < 10, do: "0#{n}"
+  defp pad2(n), do: "#{n}"
 end
